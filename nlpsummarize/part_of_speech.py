783bc77f48a1b17a60a0bbcb686f9d5c85bc331c
Karlos Muradyan <carlosmuradyan2@gmail.com>
2020-02-26 20:43:01 -0800

    Added documentation for the part_of_speech function

---
+++ nlpsummarize/part_of_speech.py
@@ -0,0 +1,32 @@
+# author: Karlos Muradyan
+# date: 26 Feb 2020
+
+def get_part_of_speech(pd_df_col):
+    '''
+    This function generates statistics about the proportions of following
+    parts of speech in the given column::
+        - verbs
+        - prepositions
+        - adjectives
+        - nouns
+        - articles
+    ------------
+    Argument
+        pd_df_col (pd.Series): column of pandas dataframe (i.e. Series)
+                        containing text data.
+    ------------
+    Return
+        pd.DataFrame with columns verbs, prepositions, adjectives, nouns, articles
+    ------------
+    Example
+        >>> ex = pd.DataFrame({'text_col' : ['Today is a beautiful Monday
+                                         and I would love getting a 
+                                         coffee. However, startbucks 
+                                         is closed.']})
+        >>> summary_4(ex['text_col'])
+        [1]  |   verbs    | prepositions | adjectives |   nouns   |  articles  |
+             |    0.2     |     0.11     |     0.3    |    0.06   |     0.18   |
+    ------------
+    '''
+
+    pass
