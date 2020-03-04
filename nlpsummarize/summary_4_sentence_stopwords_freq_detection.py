e4fae9a865cbe3eafe9f6699366c685f7a290068
vchand01 <vchand01>
2020-03-03 18:58:45 -0800

    Completed function doc

--- nlpsummarize/summary_4_sentence_stopwords_freq_detection.py
+++ nlpsummarize/summary_4_sentence_stopwords_freq_detection.py
@@ -1,7 +1,16 @@
 # Author : Vignesh Chandrasekaran 
 # Date : 26-Feb-2020
 
-def summary_4_sentence_stopwords_freq_detection(pd_df_col):
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.tokenize import sent_tokenize
+import pandas as pd
+from nltk.corpus import stopwords
+
+from itertools import islice
+
+def summary_4(pd_df_col, nof=3):
     '''
     This function generates the following:
         - number of sentences, 
@@ -28,14 +37,43 @@ def summary_4_sentence_stopwords_freq_detection(pd_df_col):
                                          coffee. However, startbucks 
                                          is closed.']})
         
-        >>> summary_4_sentence_stopwords_freq_detection(ex['text_col'])
+        >>> sentence_stopwords_freq_detection(ex['text_col'])
         
         [1]  | number of sentences | number of stop words | high freq. words |
              |         2           |           6          |    is(2), a(2)   |    
     ------------
     
     '''
+    
+    #Concatenate all the sentences. Defaults a '.' when going from one row to another.
+    all_messages = df['text'].str.cat(sep='. ')
+    
+    #Computes the number of sentences
+    number_of_sentences = len(sent_tokenize(all_messages))
+    
+    #Computes number of stop words
+    stop = len(stopwords.words('english'))
+    
+    #Splits the entire paragraph into individual words
+    word_list = all_messages.split()
+    wordfreq = []
+    for w in word_list:
+        wordfreq.append(word_list.count(w))    
+    wordfreq_pair = dict((list(zip(word_list, wordfreq))))
+    
+    #Sorts the dictionary by frequency.
+    aux = [(wordfreq_pair[key], key) for key in wordfreq_pair]
+    aux.sort()
+    aux.reverse()
+    
+    #Extracts the first nof number of words
+    freq = aux[0:nof]
+    
+    #Returns a pandas dataframe
+    return pd.DataFrame({ 'Number of sentences' : number_of_sentences,
+                         'Stop words' : stop, 
+                         'Frequency' : [freq]})
 
 
-
-
+df = pd.read_excel('../data/text_data.xlsx')
+summary_4(df['text'])
